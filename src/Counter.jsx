5e00a87b3129b5a38200b73ae44e41d9bac4e281
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-26 09:56:13 -0600

    Added states to Counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = (c) => c + 1;
+  const decrease = (c) => c - 1;
+
   return <span>Counter</span>;
 }
 export default Counter;
