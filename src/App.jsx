3322ec7c96f7ea3dcfa12ac669967bf41eb5b18b
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-09-26 11:46:22 -0600

    Refactor Counter component to use Context API and introduce compound component pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Counter from "./Counter";
 import "./styles.css";
 
@@ -5,14 +6,14 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/*       <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
     </div>
   );
 }
